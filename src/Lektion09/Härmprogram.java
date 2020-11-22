dd0df3d2a84ff6a005719f14d09994cd922fda93
lisaramel <lisa.ramel@yh.nackademin.se>
2020-11-22 12:47:56 +0100

    First commit Härmprogram

---
+++ src/Lektion09/Härmprogram.java
@@ -0,0 +1,33 @@
+package Lektion09;
+
+import java.util.Scanner;
+
+/**
+ * Created by Lisa Ramel
+ * Date: 2020-10-07
+ * Time: 09:45
+ * Project: Objektorienterad programmering och Java
+ * Copywrite: MIT
+ */
+public class Härmprogram {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in, "utf-8");
+        //sc.useDelimiter(" ");
+        System.out.print("Skriv något: ");
+
+        while(sc.hasNextLine()) {
+            String input = sc.nextLine();
+            if(input.equals("bye")){
+                System.exit(0);
+            }
+
+            System.out.println("Du skrev: " + "\"" + input + "\"");
+            System.out.println();
+
+            System.out.print("Skriv något: ");
+
+        }
+    }
+}
